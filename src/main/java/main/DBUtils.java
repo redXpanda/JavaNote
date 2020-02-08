aacd6f68a0864de8f4279743d4b2cc3944d55069
redXpanda <tgyezitang@163.com>
2020-02-08 14:45:46 +0800

    update

---
+++ src/main/java/main/DBUtils.java
@@ -0,0 +1,76 @@
+package main;
+
+import java.sql.*;
+
+public class DBUtils {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+
+
+    public static void con(String url, String user, String pwd){
+
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            // 注册 JDBC 驱动
+            Class.forName(JDBC_DRIVER);
+
+            // 打开链接
+            System.out.println("连接数据库...");
+
+            //第一种，user pwd写在函数参数里
+            //conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            //第一种，user pwd写在URL里
+            //conn = DriverManager.getConnection(DB_URL_FULL);
+            //DB_URL_FULL=jdbc:mysql://0.0.0.0:3306/DB?user=hello&password=123456"
+
+
+
+
+
+            // 执行查询
+            System.out.println(" 实例化Statement对象...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT id, name, url FROM websites";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            // 展开结果集数据库
+            while(rs.next()){
+                // 通过字段检索
+                int id  = rs.getInt("id");
+                String name = rs.getString("name");
+                String turl = rs.getString("url");
+
+                // 输出数据
+                System.out.print("ID: " + id);
+                System.out.print(", 站点名称: " + name);
+                System.out.print(", 站点 URL: " + turl);
+                System.out.print("\n");
+            }
+            // 完成后关闭
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            // 处理 JDBC 错误
+            se.printStackTrace();
+        }catch(Exception e){
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+        }finally{
+            // 关闭资源
+            try{
+                if(stmt!=null) stmt.close();
+            }catch(SQLException se2){
+            }// 什么都不做
+            try{
+                if(conn!=null) conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+    }
+
+
+}
---
+++ src/main/java/main/main.java
@@ -0,0 +1,18 @@
+package main;
+
+
+import java.sql.*;
+
+public class main {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+
+
+        System.out.println("Goodbye!");
+
+    }
+}
